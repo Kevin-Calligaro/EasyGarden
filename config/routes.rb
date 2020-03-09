7b0759455f387400c4a255cc0fec5d224df44480
kevin-calligaro <calligaro.kevin@gmail.com>
2020-03-09 17:39:12 +0100

    starting rails app

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :plants, only: [:create, :show, :destroy, :edit, :update] do
+    member do
+      patch 'seeded'
+      patch 'planted'
+      patch 'harvested'
+    end
+  end
+
+  resources :gardens, only: [:show, :create]
+  resources :vegetables, only: [:show, :index]
+
+
 end
+
