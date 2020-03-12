7f7dc5d37e423d7840cab750c91aa01eeca4222c
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-12 08:06:02 +0100

    testing plugin

---
+++ app/javascript/plugins/garden.js
@@ -0,0 +1,27 @@
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
+
+
+const cloning = () => {
+new Sortable(example3Left, {
+    group: {
+        name: 'shared',
+        pull: 'clone'
+    },
+    // animation: 150
+});
+
+new Sortable(example3Right, {
+    group: {
+        name: 'shared',
+        pull: 'clone'
+    },
+    // animation: 150
+});
+};
+
+
+
+
+
+// export { initSortable };
+export { cloning };
