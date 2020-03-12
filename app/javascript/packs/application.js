963a6fb60cdede7f915c4a75db57fa3020316188
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-12 19:25:46 +0100

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import "bootstrap";
-// import { cloning } from '../plugins/garden';
+import { persisteData } from '../persiste';
 
 // cloning();
 
 import { PluginsCollidable } from '../plugins/garden_draggable.js';
 
 PluginsCollidable();
+persisteData();
