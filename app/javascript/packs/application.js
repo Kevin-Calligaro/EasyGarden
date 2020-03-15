36bc9c6a00683aeb1e9c45a7c22d35d73dd82455
Kevin-Calligaro <58590658+Kevin-Calligaro@users.noreply.github.com>
2020-03-15 13:05:26 +0100

    Revert "Add photo vege"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 
-import "../plugins/description_edit";
-import "../plugins/description_show";
+import "../plugins/show_edit";
 // import { cloning } from '../plugins/garden';
 import { persisteData } from '../persiste';
 
