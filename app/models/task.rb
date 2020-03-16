6a89a9e74c47f6a70780573692c8b69b065143be
Clement Lucien <clement.lucien1@gmail.com>
2020-03-16 10:36:37 +0100

    Save des dates

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,19 +5,34 @@ class Task < ApplicationRecord
   def mark_as_done!
     self.done = true
     self.date = DateTime.now
+    self.save!
 
-    set_due_dates_for_following_tasks if self.action.included_in? STARTING_ACTIONS
+    set_following_tasks_due_dates if STARTING_ACTIONS.include? self.action
   end
 
   def followings_tasks
     self.garden_vegetable.tasks.where.not(id: self.id).where("step > ? ", self.step).order(step: :asc)
   end
 
-
   private
 
   def set_following_tasks_due_dates
-    if self.where(step: 1).action == "seeding"
-      self.where(step: 2).date = self.where(step: 1).date + (self.garden_vegetable.vegetable.planting_start)
+    if self.action == "planting" && self.step == 1
+      harvesting = followings_tasks.find_by(step: 2)
+      harvesting.date = self.date + (self.garden_vegetable.vegetable.harvesting_start - self.garden_vegetable.vegetable.planting_start).days
+      harvesting.save!
+    elsif self.action == "seeding" && self.step == 1 && followings_tasks.find_by(step: 2).action == "harvesting"
+      harvesting = followings_tasks.find_by(step: 2)
+      harvesting.date = self.date + (self.garden_vegetable.vegetable.harvesting_start - self.garden_vegetable.vegetable.seeding_start).days
+      harvesting.save!
+    else
+      planting = followings_tasks.find_by(step: 2)
+      planting.date = self.date + (self.garden_vegetable.vegetable.planting_start - self.garden_vegetable.vegetable.seeding_start).days
+      planting.save!
+
+      harvesting = followings_tasks.find_by(step: 3)
+      harvesting.date = self.date + (self.garden_vegetable.vegetable.harvesting_start - self.garden_vegetable.vegetable.seeding_start).days
+      harvesting.save!
+    end
   end
 end
