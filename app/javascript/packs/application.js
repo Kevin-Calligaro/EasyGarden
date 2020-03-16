b9ebeb433bfd98005bdfa7721b079f99f6f427fb
kevin-calligaro <calligaro.kevin@gmail.com>
2020-03-16 10:05:56 +0100

    begining of js for description-show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 
 import "../plugins/show_edit";
+import "../plugins/description_show.js";
 // import { cloning } from '../plugins/garden';
 import { persisteData } from '../persiste';
 
